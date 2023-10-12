df0d68fce17278bd9a046a1fe619f4e81cdc29ab
SOURAV GARAI <101336956+Garaisourav12@users.noreply.github.com>
2023-10-12 15:55:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,42 @@
 //complete this code
-class Animal {}
+class Animal {
+  constructor(species) {
+    this._species = species;
+  }
 
-class Dog extends Animal {}
+  get species() {
+    return this._species;
+  }
 
-class Cat extends Animal {}
+  makeSound() {
+    console.log('Sound');
+  }
+}
+
+class Cat extends Animal {
+  purr() {
+    console.log("purr");
+  }
+}
+
+class Dog extends Animal {
+  bark() {
+    console.log("woof");
+  }
+}
+
+// Create instances of the Cat and Dog classes
+const cat = new Cat("Domestic Cat");
+const dog = new Dog("German Shepherd");
+
+// Access the species using the getter
+console.log(`Cat species: ${cat.species}`);
+console.log(`Dog species: ${dog.species}`);
+
+// Make sounds
+cat.purr(); // Outputs: purr
+dog.bark(); // Outputs: woof
+
 
 // Do not change the code below this line
 window.Animal = Animal;
